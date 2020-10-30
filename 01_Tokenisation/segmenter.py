c63ef231ad3dcc8b0b16bc56134a696affb25ec5
hoffmjac <hoffmjac@iu.edu>
2020-10-29 20:58:26 -0400

    commit

--- 01_Tokenisation/segmenter.py
+++ 01_Tokenisation/segmenter.py
@@ -4,10 +4,9 @@ text = sys.stdin.read()
 
 text = text.replace('. ', '.\n')
 
+text = text.replace('\n\n', '\n')
+
+text = text[:-1]
+
 print(text)
-
-
-
-
-
 
--- 01_Tokenisation/tokeniser.py
+++ 01_Tokenisation/tokeniser.py
@@ -1,13 +1,41 @@
-import sys
-
-text = sys.stdin.read()
-
-text = text.replace('.', '\n.')
+import sys, re
 
 
+underscoremania = "\t_\t_\t_\t_\t_\t_\t_\t_"
 
-text = text.replace(' ', '\n')
+abbr = ['etc.', 'e.g.', 'i.e.']
+
+id = 0
+
+line = sys.stdin.readline()
+while line:
+	id += 1
+	print('# sent_id =', id)
+	print("# text =", line[:-1])
+	line = re.sub(r'([\(\)"?:!;])', r' \g<1> ', line)
+	line = re.sub(r'([^0-9]),', r'\g<1> ,', line)
+	line = re.sub(r',([^0-9])', r', \g<1>', line)
+	line = re.sub(r'  +', ' ', line[:-1])
+
+	line = line.split()
 
 
-print(text)
+	count = 0
+	for entry in line:
+		if entry[-1] == '.' and entry not in abbr:
+			count += 1
+			a = str(count) + '\t' + entry[:-1] + underscoremania
+			print(a)
+			count += 1
+			b = str(count) + '\t' + '.' + underscoremania
+			print(b)
+		else:
+			count += 1
+			entry = str(count) + "\t" + entry + underscoremania
+			print(entry)
+	print()
+	line = sys.stdin.readline()
+
+
+print(line)
 
